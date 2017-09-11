7cd9a560a94dce4a2635342024f9b9afe0e26417
rcarv-rafael <rcarv.rafael@gmail.com>
2017-09-11 11:25:34 -0300

    Update hooks.rb
    adicionando exemplo de controle de evidencia visual

--- FrontEnd/features/support/hooks.rb
+++ FrontEnd/features/support/hooks.rb
@@ -1,8 +1,19 @@
-
-AfterStep do |step|
-    
+Before do |scenario|
+  @feature_name = scenario.feature.name
+  @scenario_name = scenario.name
 end
 
+AfterStep do |step|
+   #setando no prompt de comando a variavel nivel_evidencia, pode controlar te ou não uma evidencia a cada passo
+  if ENV['nivel_evidencia'] == 'step'
+    screenshot =  "features/reports/screenshots/#{@feature_name}/#{@scenario_name}/#{DateTime.now}.png"
+    page.save_screenshot(screenshot)
+    tempimg = File.open(screenshot, "rb")
+    encoded_img = Base64.encode64(tempimg.read)
+    embed("data:image/png;base64,#{encoded_img}",'image/png', "Evidencia visual: #{screenshot}")
+    tempimg.close
+  end
+end
 
 After do |scenario|
   if scenario.failed?
