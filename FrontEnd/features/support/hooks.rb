54f21c5cae1ac9458f3556d4023cd95b16a3f47d
rcarv-rafael <rcarv.rafael@gmail.com>
2017-11-28 10:31:13 -0200

    Update hooks.rb
    Adicionando saída com o o nome do step no arquivo de evidencia

--- FrontEnd/features/support/hooks.rb
+++ FrontEnd/features/support/hooks.rb
@@ -26,3 +26,30 @@ After do |scenario|
   end
 
 end
+
+# Para utilizar o nome do step na saída, pode ser utilizada a seguinte base de hooks:
+Before do |scenario|
+  @steps_count=0
+  @feature_steps= []
+end
+
+AfterStep do |step|
+  @steps_count+=1
+end
+
+After do |scenario|
+  if scenario.failed?
+    scenario.test_steps.each { |step_element| @feature_steps << step_element if step_element.location[:file].match("#{scenario.feature.file}") }
+    @name_step = @feature_steps[@steps_count].name.gsub("/","")
+
+    puts "Falha no step: " + @name_step
+    
+    screenshot =  "#{scenario.name}_#{@name_step}.png"
+    
+    page.save_screenshot(screenshot)
+    tempimg = File.open(screenshot, "rb")
+    encoded_img = Base64.encode64(tempimg.read)
+    embed("data:image/png;base64,#{encoded_img}",'image/png', "Screenshot Error: #{screenshot}")
+    tempimg.close    
+  end
+end
